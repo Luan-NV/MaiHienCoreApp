8b25b9c8365042036a6b8fa95310b1290906e84a
LuanNV <luanvne@outlook.com>
2018-07-12 19:39:48 +0700

    display category on treeview

---
+++ MaiHienCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using MaiHienCoreApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaiHienCoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        private IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Get Data API
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+
+        #endregion Get Data API
+    }
+}
